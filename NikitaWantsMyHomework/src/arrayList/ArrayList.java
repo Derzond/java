b253d75658f397c1fa7eba21aac2752384d6bfd0
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-11-22 15:07:23 +0300

    Added review comments

--- NikitaWantsMyHomework/src/arrayList/ArrayList.java
+++ NikitaWantsMyHomework/src/arrayList/ArrayList.java
@@ -3,8 +3,16 @@ package arrayList;
 public class ArrayList<T> implements MyArrayList<T> {
     // Стартовый размер листа.
     private static final int DEFAULT_CAPACITY = 4;
+
+    // Вметсо lastPutIndex я бы сделал currentSize, который изначально равен 0. Так не надо будет в проверке на переполнение массива что-то вычитать из чего-то,
+    // было бы чуть-чуть читабельнее, но это, кажется, уже придирка.
+
     // Индекс последнего вставленного элемента.
     private int lastPutIndex = -1;
+
+    // Идея сразу скастить массив к T[] кажется хорошей, ибо не придется при каждом get кастить элемент из Object[] к T (в отличии от моего кода).
+    // Но не совсем понял комментарий. Ошибки же не будет потому, что в array гарантированно кладется только объекты типа T.
+    // При чем тут возвращения массива пользователю?
 
     // Нет ошибки, т.к массив никогда не будет возвращен пользователю.
     @SuppressWarnings("unchecked")
@@ -33,6 +41,7 @@ public class ArrayList<T> implements MyArrayList<T> {
      * @param value значение которое нужно добавить.
      */
     public void put(T value){
+        // Можно было array[++lastPutIndex] = value;
         array[lastPutIndex + 1] = value;
         ++lastPutIndex;
 
@@ -48,6 +57,7 @@ public class ArrayList<T> implements MyArrayList<T> {
      */
     public boolean contains(T value){
         for (int i = 0; i < lastPutIndex + 1; i++)
+            // Надо было использовать array[i].equals(value), ибо через == идет сравнение по ссылкам.
             if (array[i] == value)
                 return true;
 
@@ -75,6 +85,9 @@ public class ArrayList<T> implements MyArrayList<T> {
      * Увеличивает размер листа в 2 раза.
      */
     private void resize(){
+        // Постоянным умножением на 2 массив может сильно распухнуть.
+        // Например, хочу вставить (2^10 + 1)-й элемент и закончить на этом работу с массивом, а он фиганул массив на 2^11 элементов.
+
         // Нет ошибки, т.к массив никогда не будет возвращен пользователю.
         @SuppressWarnings("unchecked")
         T[] temp = (T[])new Object[array.length * 2];
